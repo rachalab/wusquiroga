b47dd87817b98e725086b39620dc274e0d633ef9
MillandStudio <millandstudio@gmail.com>
2025-07-03 21:26:47 -0300

    Sheet

--- src/components/builder/Sheet/Sheet.jsx
+++ src/components/builder/Sheet/Sheet.jsx
@@ -1,27 +1,29 @@
 "use client";
 
 import Modal from "@components/structure/Modal/Modal";
+import colorSchemas from "@styles/colorSchemas.module.scss";
+
 import { useState } from "react";
 
 import styles from "./Sheet.module.scss";
 
-function Sheet({ title, cta, viewMore, item }) {
+function Sheet({ title, cta, viewMore, item, colorschema }) {
   const showMore = item?.length > 4;
   const visibleItems = showMore ? item.slice(0, 4) : item;
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   return (
-    <div className={styles.sheet}>
+    <div className={`${styles.sheet} ${colorSchemas[colorschema]}`}>
       {title && <h3 className={styles.sheetTitle}>{title}</h3>}
-
       <div className={styles.sheetContent}>
         <div className={styles.sheetItems}>
-          {visibleItems.map((entry, index) => (
-            <div key={index} className={styles.sheetItem}>
-              <h5>{entry.title}</h5>
-              <div dangerouslySetInnerHTML={{ __html: entry.text }} />
-            </div>
-          ))}
+          {visibleItems?.length > 0 &&
+            visibleItems.map((entry, index) => (
+              <div key={index} className={styles.sheetItem}>
+                <h5>{entry.title}</h5>
+                <div dangerouslySetInnerHTML={{ __html: entry.text }} />
+              </div>
+            ))}
         </div>
 
         {viewMore && cta && (
@@ -30,19 +32,19 @@ function Sheet({ title, cta, viewMore, item }) {
           </div>
         )}
       </div>
-
       <Modal
         title={title}
         isOpen={isModalOpen}
         onClose={() => setIsModalOpen(false)}
       >
         <div className={styles.sheetItemsModal}>
-          {item.map((entry, index) => (
-            <div key={index} className={styles.sheetItem}>
-              <h5>{entry.title}</h5>
-              <div dangerouslySetInnerHTML={{ __html: entry.text }} />
-            </div>
-          ))}
+          {item?.length > 0 &&
+            item.map((entry, index) => (
+              <div key={index} className={styles.sheetItem}>
+                <h5>{entry.title}</h5>
+                <div dangerouslySetInnerHTML={{ __html: entry.text }} />
+              </div>
+            ))}
         </div>
       </Modal>
     </div>
