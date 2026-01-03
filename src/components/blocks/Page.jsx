f0c9f220b706f2ef6d1015815bc57a1adeb9d2fd
Christian Milillo (CA) <christian.milillo@courtavenue.com>
2026-01-03 16:06:06 -0300

    Next update

--- src/components/blocks/Page.jsx
+++ src/components/blocks/Page.jsx
@@ -1,11 +1,14 @@
-import { StoryblokComponent, storyblokEditable } from "@storyblok/react/rsc";
+import {
+    storyblokEditable,
+    StoryblokServerComponent,
+} from '@storyblok/react/rsc';
 
-const Page = ({ blok }) => (
-    <main >
-        {blok.body?.map((nestedBlok) => (
-            <StoryblokComponent blok={nestedBlok} key={nestedBlok._uid} />
-        ))}
-    </main>
-);
-
-export default Page;
+export default function Page({ blok }) {
+    return (
+        <main>
+            {blok.body?.map((nestedBlok) => (
+                <StoryblokServerComponent blok={nestedBlok} key={nestedBlok._uid} />
+            ))}
+        </main>
+    );
+}
